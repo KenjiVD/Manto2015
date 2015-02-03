443185a857d3f236779a3f69d2b5b3c05aa749fd
KenjiVD <idsemanuelupch@gmail.com>
2015-02-03 17:32:25 -0600

    Inicio parte 1

---
+++ src/edu/uag/iidis/scec/modelo/Pais.java
@@ -0,0 +1,86 @@
+package edu.uag.iidis.scec.modelo;
+
+import java.io.Serializable;
+import java.util.*;
+
+
+/**
+ * Esta clase es usada para representar un rol de un
+ * usuario. 
+ *
+ * <p><a href="Pais.java.html"><i>Ver código fuente</i></a></p>
+ *
+ * @author <a href="mailto:vramos@uag.mx">Victor Ramos</a>
+ * @version 1.0
+ */
+public class Pais extends ClaseBase 
+        implements Serializable {
+
+    private Long id;
+    private String descripcion;
+    private List usuarios = new ArrayList();
+
+    public Pais() {
+    }
+
+    public Pais(Long id){
+        this.id = id;
+    }
+
+    public Pais(String descripcion){
+        this.descripcion=descripcion;
+    }
+
+    /**
+     * Regresa el id del rol.
+     * @return Long
+     */
+    public Long getId() {
+        return this.id;
+    }
+
+    /**
+     * Establece el id del rol.
+     * @return void
+     */
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    
+    /**
+     * Regresa la descripción del rol.
+     * @return String
+     */
+    public String getDescripcion() {
+        return this.descripcion;
+    }
+
+    /**
+     * Establece la descripción del rol.
+     * @return void
+     */
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    /**
+     * Regresa los usuarios del rol.
+     * @return List
+     */
+    public List getUsuarios() {
+        return this.usuarios;
+    }
+
+    /**
+     * Establece los usuarios del rol.
+     * @return void
+     */
+    public void setUsuarios(List usuarios) {
+        this.usuarios = usuarios;
+    }
+
+    public void addUsuario(Usuario usuario) {
+        usuarios.add(usuario);
+        usuario.getEstados().add(this);
+    }
+}
---
+++ web/WEB-INF/vista/comun/banner.jsp
@@ -0,0 +1,10 @@
+
+	<!-- IS: Encabezado de Pagina (Banner) -->
+	 <section class="barraBanner2">
+		<span>IDS</span>
+		<span id="big">Validador de Clientes</span>
+		<span>UPCH</span>
+	</section>
+	<!-- FS: Encabezado de Pagina (Banner) -->					
+
+					
