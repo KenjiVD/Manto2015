638d57fb13130128d3cd4de6ecd2b8dfa0265e49
KenjiVD <idsemanuelupch@gmail.com>
2015-03-21 18:50:57 -0600

    CompoBox ListaTest - Vista Nueva Seccion y Vista Nueva Pregunta - Por Corregir

--- src/edu/uag/iidis/scec/control/MCURegistrarSeccion.java
+++ src/edu/uag/iidis/scec/control/MCURegistrarSeccion.java
@@ -33,18 +33,41 @@ public final class MCURegistrarSeccion
                 HttpServletResponse response)
             throws Exception {
 
+        if (log.isDebugEnabled()) {
+            log.debug(">solicitarListarTest");
+        }
+
+        // Verifica si la acción fue cancelada por el usuario
+        if (isCancelled(request)) {
+            if (log.isDebugEnabled()) {
+                log.debug("<La acción fue cancelada");
+            }
+            return (mapping.findForward("cancelar"));
+        }
+
         FormaNuevaSeccion forma = (FormaNuevaSeccion)form;
 
         ManejadorTest mr = new ManejadorTest();
         Collection resultado = mr.listarTest();
 
-        forma.setTest( resultado );
-
-        if (log.isDebugEnabled()) {
-            log.debug(">solicitarRegistroSeccion");
+        ActionMessages errores = new ActionMessages();
+        if (resultado != null) {
+            if ( resultado.isEmpty() ) {
+                errores.add(ActionMessages.GLOBAL_MESSAGE,
+                    new ActionMessage("errors.registroVacio"));
+                saveErrors(request, errores);
+            } else {
+                forma.setTest( resultado );
+            }
+            return (mapping.findForward("exito"));
+        } else {
+            log.error("Ocurrió un error de infraestructura");
+            errores.add(ActionMessages.GLOBAL_MESSAGE,
+                        new ActionMessage("errors.infraestructura"));                
+            saveErrors(request, errores);
+            return ( mapping.findForward("fracaso") );
         }
 
-        return (mapping.findForward("exito"));
     }
 
 
